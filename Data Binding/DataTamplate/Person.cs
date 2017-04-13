bd4b3b707b3f991f7920b925a9b50fbaae501076
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-04-13 23:25:01 +0400

    tamplete

---
+++ Data Binding/DataTamplate/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTamplate
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+
+        public Person(string firstName, string lastName, int age, string position)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Position = position;
+        }
+    }
+
+    public class PersonList : List<Person>
+    {
+        public PersonList()
+        {
+            this.Add(new Person("Tigran", "Vardanyan", 28, "Developer"));
+            this.Add(new Person("Artur", "Mkrtchyan", 40, "Lecturer"));
+            this.Add(new Person("Areg", "Gevorgyan", 27, "Manager"));
+        }
+    }
+}
