eb898397ec13ce1eda4d6eede8b3724fca50323a
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-02 19:51:09 +0400

    Simple PeerChat

---
+++ WCF_WPF_Chat_Test_Project/WpfServer/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.PeerResolvers;
+
+namespace WpfServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
