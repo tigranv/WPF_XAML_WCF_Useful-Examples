57ff1e77966b3ec51bf5f4b4d5d17b3649db58b3
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-09 15:46:50 +0400

    cleaned

--- Lesson2WCF/Wcf2/IService1.cs
+++ Lesson2WCF/Wcf2/IService1.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
+﻿using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.Text;
 
 namespace Wcf2
 {
