43046811ece910e3424641a1bff274742f1adefd
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-09 15:48:53 +0400

    cleaned

--- NET.Tcp_Test/WpfServerNetTCP/MainWindow.xaml.cs
+++ NET.Tcp_Test/WpfServerNetTCP/MainWindow.xaml.cs
@@ -2,28 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.ServiceModel;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfServerNetTCP
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
-
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
     public partial class MainWindow : Window, IMessage
     {
