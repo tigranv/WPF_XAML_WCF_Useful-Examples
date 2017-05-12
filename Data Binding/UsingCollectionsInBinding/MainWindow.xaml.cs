d7b8d882bbaa1a43bf616c3dd1518531e7833590
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-12 12:05:42 +0400

    simplified

--- Data Binding/UsingCollectionsInBinding/MainWindow.xaml.cs
+++ Data Binding/UsingCollectionsInBinding/MainWindow.xaml.cs
@@ -4,9 +4,6 @@ using System.Windows;
 
 namespace UsingCollectionsInBinding
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         ObservableCollection<string> list = new ObservableCollection<string>();
