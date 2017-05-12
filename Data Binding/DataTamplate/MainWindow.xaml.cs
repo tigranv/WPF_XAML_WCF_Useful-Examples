1a05c70c9bf9100b4702bedb62463e5145f5c7b4
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-12 12:06:17 +0400

    cleaned

--- Data Binding/DataTamplate/MainWindow.xaml.cs
+++ Data Binding/DataTamplate/MainWindow.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace DataTamplate
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
