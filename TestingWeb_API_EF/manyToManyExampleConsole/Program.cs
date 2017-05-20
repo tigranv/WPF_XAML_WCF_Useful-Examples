7d27e9a058a6b9ab7a058a0d8dbbbf47b3f52b97
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-20 19:04:31 +0400

    db created

---
+++ TestingWeb_API_EF/manyToManyExampleConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manyToManyExampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCodefirstExample db = new MyCodefirstExample();
+            db.Orders.ToList();
+        }
+    }
+}
