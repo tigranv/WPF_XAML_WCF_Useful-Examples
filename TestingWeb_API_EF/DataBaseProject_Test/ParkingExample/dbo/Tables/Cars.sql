ddb0bcad98602a18e5468c5de98767d0cbce64d3
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-20 13:38:25 +0400

    test dbo added

---
+++ TestingWeb_API_EF/DataBaseProject_Test/ParkingExample/dbo/Tables/Cars.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Cars] (
+    [Id]    INT           IDENTITY (1, 1) NOT NULL,
+    [Mark]  VARCHAR (100) NULL,
+    [Model] VARCHAR (100) NULL,
+    [year]  INT           NULL,
+    CONSTRAINT [PK_Cars] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
