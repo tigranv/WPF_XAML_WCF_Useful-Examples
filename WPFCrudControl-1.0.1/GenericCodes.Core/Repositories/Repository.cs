4acfcbc950e6ca207758c1be18db0d3f621670b8
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-11-01 22:54:32 +0400

    core

---
+++ WPFCrudControl-1.0.1/GenericCodes.Core/Repositories/Repository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using GenericCodes.Core.DbContext;
+using GenericCodes.Core.Entities;
+using Microsoft.Practices.ServiceLocation;
+
+namespace GenericCodes.Core.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public TEntity Find(params object[] keyValues)
+        {
+            return _dbContext.Set<TEntity>().Find(keyValues);
+        }
+
+        public virtual IEnumerable<TEntity> List()
+        {
+            return _dbContext.Set<TEntity>().AsEnumerable();
+        }
+
+        public virtual IEnumerable<TEntity> List(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbContext.Set<TEntity>()
+                   .Where(predicate)
+                   .AsEnumerable();
+        }
+
+        public void Insert(TEntity entity)
+        {
+            Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Added;
+        }
+
+        public void InsertRange(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                Insert(entity);
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            var dbset = _dbContext.Set<TEntity>();
+            var entry = _dbContext.Entry(entity);
+            if (entry.State == EntityState.Detached)
+            {
+                Attach(entity);
+            }
+            dbset.Remove(entity);
+        }
+
+        public void Delete(object id)
+        {
+            var entity = _dbContext.Set<TEntity>().Find(id);
+            Delete(entity);
+        }
+
+        public IQueryable<TEntity> Queryable()
+        {
+            return _dbContext.Set<TEntity>();
+        }
+
+        private void Attach(TEntity entity)
+        {
+            if (_dbContext.Entry(entity).State == EntityState.Detached)
+            {
+                _dbContext.Set<TEntity>().Attach(entity);
+            }
+        }
+    }
+}
---
+++ WPFCrudControl-1.0.1/Northwind.Demo/ViewModel/Products/ProductsListViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using GenericCodes.CRUD.WPF.ViewModel.CRUDBases;
+using Northwind.DAL.Models;
+using Northwind.Service.Interfaces;
+
+namespace Northwind.Demo.ViewModel.Products
+{
+    public class ProductsListViewModel : GenericCrudBase<Product>
+    {
+        public ProductsListViewModel(ProductsSearchViewModel productsSearchViewModel,
+            ProductAddEditViewModel productAddEditViewModel, ICategoryService categoryService, ISuppliersService suppliersService)
+            : base(productsSearchViewModel, productAddEditViewModel)
+        {
+            ListingIncludes = new Expression<Func<Product, object>>[]
+            {
+                p => p.Category,
+                p => p.Supplier
+            };
+
+            PreAddEditDelegate = (type) =>
+            {
+                productAddEditViewModel.Categories = categoryService.GetALL();
+                productAddEditViewModel.Suppliers = suppliersService.GetALL();
+            };
+        }
+    }
+}
